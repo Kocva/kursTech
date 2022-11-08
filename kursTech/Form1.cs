5fe1ee4e4a3fd4157993d194cf5a3e2106290ed7
Konstantin kazaryan <kocva2003@mail.ru>
2022-11-09 00:02:48 +0800

    ADD list

--- kursTech/Form1.cs
+++ kursTech/Form1.cs
@@ -12,10 +12,19 @@ namespace kursTech
 {
     public partial class Form1 : Form
     {
+        List<Particle> particles = new List<Particle>();
         public Form1()
         {
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            for (var i = 0; i < 500; i++)
+            {
+                var particle = new Particle();
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                particles.Add(particle);
+            }
         }
     }
 }
